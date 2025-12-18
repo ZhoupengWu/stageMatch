7b63a648b2a89a1b8890a717a10d4665800fa75b
BERTULETTI RICCARDO <BERTULETTI.22170>
2025-12-18 14:57:59 +0100

    BERTULETII: miglioramento interfaccia grafica ma da sistemare...

--- index.js
+++ index.js
@@ -1,8 +1,155 @@
-const map = L.map('map').setView([45.7696, 14.6483], 13);
+const button = document.getElementById("percorso");
 
-L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributori'
+const input_address_start = document.getElementById("address_start");
+const input_address_end = document.getElementById("address_end");
+const initial_coordinates = [45.695, 9.67];
+
+const map = L.map("map").setView([initial_coordinates[0], initial_coordinates[1]], 13);
+const intre = L.marker([45.592, 9.301]).addTo(map);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributori',
 }).addTo(map);
 
-const marker = L.marker([41.9028, 12.4964]).addTo(map);
-marker.bindPopup("<b>Ciao!</b><br>Questa è Roma.").openPopup();
+const marker = L.marker([45.695, 9.67]).addTo(map);
+marker.bindPopup('<a href="www.google.com">Questa è Bergamo</a>').openPopup();
+
+async function calcolaPercorso() {
+    const API_KEY = "eyJvcmciOiI1YjNjZTM1OTc4NTExMTAwMDFjZjYyNDgiLCJpZCI6IjY5NzY4MGNiZWViZjQ5MGQ4ZjNhNWFiZjBkZTBmZGMxIiwiaCI6Im11cm11cjY0In0=";
+
+    // coordinate in formato [lon, lat]
+    const start = [9.66, 45.697];
+    const end = [9.703, 45.672];
+
+    const response = await fetch( "https://api.openrouteservice.org/v2/directions/driving-car/geojson", {
+        method: "POST",
+        headers: {
+            Authorization: API_KEY,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            coordinates: [
+                start,
+                end
+            ]
+        })
+    });
+
+    if (!response.ok) {
+        throw new Error(`Code error ${response.status}`);
+
+    }
+
+    const data = await response.json();
+
+    L.geoJSON(data, { style: { color: 'red', weight: 4 } }).addTo(map);
+}
+
+button.addEventListener("click", calcolaPercorso);
+
+async function getCoordinates(addressStart, addressEnd) {
+    if (!addressStart) {
+        console.error("Devi inserire un indirizzo di partenza!");
+        alert("Devi inserire un indirizzo di partenza!");
+
+        return;
+    }
+
+    if (!addressEnd) {
+        console.error("Devi inserire un indirizzo di arrivo!");
+        alert("Devi inserire un indirizzo di arrivo!");
+
+        return;
+    }
+
+    const urlStart = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(addressStart)}&format=json&limit=1`;
+    const urlEnd = `https://nominatim.openstreetmap.org/search?q=${encodeURIComponent(addressEnd)}&format=json&limit=1`;
+
+    let coordsStart = [];
+    let coordsEnd = [];
+
+    /*
+        Richiesta del primo indirizzo
+    */
+    try {
+        const response = await fetch(urlStart);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            coordsStart.push(lat, lon);
+        }
+        else {
+            console.error("Qualcosa è andato storto!");
+
+            return;
+        }
+    } catch (error) {
+        console.error(error);
+        alert(error);
+
+        return;
+    }
+
+    /*
+        Richiesta del secondo indirizzo
+    */
+    try {
+        const response = await fetch(urlEnd);
+        const data = await response.json();
+
+        if (data.length > 0) {
+            const lat = data[0].lat;
+            const lon = data[0].lon;
+            coordsEnd.push(lat, lon);
+        }
+        else {
+            console.error("Qualcosa è andato storto!");
+
+            return;
+        }
+    } catch (error) {
+        console.error(error);
+        alert(error);
+
+        return;
+    }
+
+    const coords = [coordsStart, coordsEnd];
+
+    return coords;
+}
+
+async function suggestion() {
+    const address = input_address_start.value;
+
+    try {
+        const response = await fetch(
+            `photon.komoot.io/api/?q=${encodeURIComponent(address)}&lat=${initial_coordinates[0]}&lon=${initial_coordinates[1]}&limit=5&lang=it`
+        );
+
+        const data = await response.json();
+        const data_address = data.features.map((feature, index) => (
+            {
+                id: index,
+                coordinates: {
+                    longitudine: feature.geometry.coordinates[0],
+                    latitudine: feature.geometry.coordinates[1]
+                },
+                indirizzo_via: feature.properties.street,
+                indirizzo_civico: feature.properties.housenumber,
+                indirizzo_cap: feature.properties.postcode,
+                indirizzo_city: feature.properties.city
+            }
+        ));
+
+        const data_address_html = data_address.map(data => {
+            return `
+                <a href="#" id="${data.id}">${data.indirizzo_via} ${data.indirizzo_civico} ${data.indirizzo_cap} ${data.indirizzo_city}</a>
+            `
+        });
+    } catch (error) {
+        throw new Error(error);
+    }
+}
