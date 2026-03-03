cb6381977c093bbb1568c4c1ae213387254e2e46
ZhoupengWu <182263202+ZhoupengWu@users.noreply.github.com>
2026-03-03 13:59:12 +0100

    WU: inizio implementazione delle rotte protette

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
+import os
 from flask import Flask, render_template, redirect
+from middleware.sso_middleware import SSOMiddleware
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = Flask(
     __name__,
     static_folder="./resources",
     template_folder="./resources"
+)
+
+sso_middleware = SSOMiddleware(
+    jwt_secret=os.getenv("JWT_SECRET"),
+    jwt_audience=os.getenv("APP_AUDIENCE"),
+    portal_url=os.getenv("PORTAL_URL")
 )
 
 @app.route('/')
@@ -15,6 +26,7 @@ def login():
     return redirect("http://127.0.0.1:3020")
 
 @app.route('/logged/map')
+@sso_middleware.sso_login_required
 def map():
     return render_template("/html/index.html")
 
